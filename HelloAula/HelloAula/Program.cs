645a98e347660e8a799f78f6d0c844763d5cc36c
Magi Asparuhova <magi.a@abv.bg>
2015-09-03 03:41:13 +0300

    Korekcia na README.md file

---
+++ HelloAula/HelloAula/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace HelloAula
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello Aula");
+
+        }
+    }
+}
