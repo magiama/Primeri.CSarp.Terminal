a4a8981cb793d3d1fe9de7d592bd48eefda06a7c
Magi Asparuhova <magi.a@abv.bg>
2015-09-04 03:03:57 +0300

    Modul02. Video05 - Chetene ot red

---
+++ ReadFromCommandLine/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+namespace ReadFromCommandLine
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            //Дефиниране променливи
+            int a = 5, b = 0;
+            
+            //Въвеждане на входни параметри
+            Console.Write("Въведете стийност за а: ");
+            b = Convert.ToInt32(Console.ReadLine());
+            
+            //Печат резултат
+            Console.WriteLine("Резултатът е:" + (a + b).ToString()+"\n\n\n");
+            Console.ReadKey();
+        }
+    }
+}
